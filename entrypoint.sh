34acea3df2baca6b9b6e8ea0d60416fa34c4fc9b
Yusuke Izawa <yuizalp@gmail.com>
2020-02-10 16:48:01 +0900

    Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@
 
 root_file="$1"
 args="$2"
+default_args="-gg -pdf -shell-escape -file-line-error -interaction=nonstopmode"
 
 echo "Compiling LaTeX sources..."
-latexmk -gg "$args" "$root_file"
+echo ""
+if [ -z "$args" ]; then
+  latexmk "$default_args" "$root_file"
+else
+  latexmk "$args" "$root_file"
+fi
 
 echo "Compiled assets: $(realpath main.pdf)"
 echo ""
