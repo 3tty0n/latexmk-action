843d8f3fc54ffa7b9a2da7f85b1921b56054e759
Yusuke Izawa <yuizalp@gmail.com>
2020-05-30 17:59:14 +0900

    Added work_dir arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,12 @@
 
 root_file="$1"
 args="$2"
+working_dir="$3"
 default_args="-gg -pdf -shell-escape -file-line-error -interaction=nonstopmode"
+
+if [ -n "$working_dir" ]; then
+  cd "$working_dir"
+fi
 
 echo "Compiling LaTeX sources..."
 echo ""
